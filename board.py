206c7ff1fc48398489edf0d6adf96e8d711e5a26
Luke Civantos <lfcivantos@gmail.com>
2018-12-25 00:24:20 -0600

    Built Board

--- board.py
+++ board.py
@@ -5,6 +5,7 @@ class Board:
     def __init__(self, height, width, numBombs): 
         self.board = {}
         self.visible = {}
+        self.height,self.width = height,width
         self.populateBoard(height,width)
         self.setBombs(numBombs)
     
@@ -12,7 +13,7 @@ class Board:
         for i in range(height): 
             for j in range(width): 
                 self.board[(i,j)] = 0
-                self.visible = 0
+                self.visible[(i,j)] = 0
 
     def setBombs(self, numBombs): 
         allKeys = self.board.keys()
@@ -25,11 +26,29 @@ class Board:
             return True 
         return False 
 
-    def displayBoard(self): 
-        pass
-    def updateBoard(self):
-        pass
+    def updateBoard(self, i, j): 
+        queue = [(i,j)]
+        while queue: 
+            node = queue.pop(0)
+            neighbors = self.getNeighbors(node[0],node[1])
+            totalBombs = 0
+            for neighbor in neighbors: 
+                if self.board[neighbor] == None: 
+                    totalBombs += 1
+                else:   
+                    queue.append(neighbor)
+            self.board[(i,j)] = totalBombs
+            self.visible[(i,j)] = 1
 
+    def getNeighbors(self, i, j): 
+        coords = [(i-1,j-1), (i-1,j), (i,j-1), (i+1,j), (i,j+1), (i+1,j+1)]
+        neighbors = []
+        for neighbor in coords: 
+            a,b = neighbor
+            if a > 0 and b > 0 and a < len(self.board) - 1 and b < len(self.board[0]) - 1: 
+                if self.visible[neighbor] == 0: 
+                    neighbors.append(neighbor)
+        return neighbors
 
 
 board = Board(80,90,15)    
--- minesweeper.py
+++ minesweeper.py
@@ -11,21 +11,15 @@ from board import Board
 
 class Minesweeper: 
     def __init__(self): 
-        self.width = 400
-        self.height = 600
+        self.width = 630
+        self.height = 405
+        self.margin = 5
         self.size = (self.width, self.height)
 
         self.runGame()
       
-    def generateGame(self): 
-        self.board = Board(80,90,15)    
-
     def runGame(self): 
-        init()
-
-        #populate screen
-        self.screen = pygame.display.set_mode(self.size)
-        self.screen.fill((0,0,0))
+        self.displayBoard()
 
         running = True
         while running: 
@@ -33,10 +27,58 @@ class Minesweeper:
                 if event.type == KEYDOWN: 
                     if event.key == K_ESCAPE: 
                         running = False
+                elif event.type == MOUSEBUTTONUP:
+                    pos = pygame.mouse.get_pos()
+                    print(pos)
                 elif event.type == QUIT: 
                     running = False
-            
-                
+
+
+    def displayBoard(self): 
+        init()
+
+        self.screen = pygame.display.set_mode(self.size)
+        pygame.display.set_caption("Minesweeper")
+
+        self.board = Board(16,25,100) 
+        self.updateBoard()
+
+    def updateBoard(self):
+        # Define some colors
+        BLACK = (0, 0, 0)
+        WHITE = (255, 255, 255)
+        GREEN = (0, 255, 0)
+        RED = (255, 0, 0)
+ 
+        # This sets the WIDTH and HEIGHT of each grid location
+        WIDTH = 20
+        HEIGHT = 20
+        
+        # This sets the margin between each cell
+        MARGIN = 5
+        
+        self.screen.fill(BLACK)
+
+        # Draw the grid
+        for row in range(16):
+            for column in range(25):
+                color = WHITE
+                if self.board.visible[(row,column)] == 1: 
+                    if self.board.board[(row,column)] == None:
+                        color = RED
+                    else: 
+                        color = GREEN
+                pygame.draw.rect(self.screen,
+                                color,
+                                [(MARGIN + WIDTH) * column + MARGIN,
+                                (MARGIN + HEIGHT) * row + MARGIN,
+                                WIDTH,
+                                HEIGHT])
+        pygame.display.flip()
+
+    def getCell(self, i, j):
+        pass
+
 
 
 Minesweeper()
